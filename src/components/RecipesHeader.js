f6f4ecac0b0b686ef09f906b7404ee4fece394f3
marshallhelenm <marshallhelenm@gmail.com>
2019-10-03 10:23:00 -0700

    lastminutecss

--- src/components/RecipesHeader.js
+++ src/components/RecipesHeader.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { Header, Icon } from "semantic-ui-react";
+import {Link} from 'react-router-dom'
 
 class RecipesHeader extends Component {
   render() {
     return (
-      <div id="recipes-header">
+      <a href='/recipes' id="recipes-header">
         <Header as="h2" icon textAlign="center">
           <Icon color="yellow" name="book" circular />
           <Header.Content>{this.props.title}</Header.Content>
         </Header>
-      </div>
+      </a>
     );
   }
 }
