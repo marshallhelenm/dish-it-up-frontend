01b9aba74cd4726f62b2399a4be3607c4f85f053
marshallhelenm <marshallhelenm@gmail.com>
2019-11-27 11:13:14 -0800

    hid most elements of navbar when not logged in

--- src/containers/NavBar2.js
+++ src/containers/NavBar2.js
@@ -13,44 +13,47 @@ export default class NavBar extends Component {
 
   render() {
     return (
-      <div id='nav-bar'>
+      <div id="nav-bar">
         <Menu color="orange" stackable>
-            <img
-              id="logo"
-              src="https://www.trzcacak.rs/myfile/detail/288-2882308_chef-man-emoji-chef-woman-png-cartoon.png"
-              alt="a happy chef"
-            />
+          <img
+            id="logo"
+            src="https://www.trzcacak.rs/myfile/detail/288-2882308_chef-man-emoji-chef-woman-png-cartoon.png"
+            alt="a happy chef"
+          />
           <Menu.Item>
-            <h2 id='logo-text' >Dish It<br></br>Up!</h2>
+            <h2 id="logo-text">
+              Dish It<br></br>Up!
+            </h2>
           </Menu.Item>
+          {this.props.loggedIn ? (
+            <>
+              <Menu.Item as={Link} to="/">
+                <Icon color="orange" name="home" size="big" />
+              </Menu.Item>
+              {/* <Menu.Item as={Link} to="/" name="Dashboard" /> */}
+              <Menu.Item as={Link} to="/recipes">
+                <Icon color="orange" name="book" size="big" />
+              </Menu.Item>
+              {/* <Menu.Item as={Link} to="/recipes" name="Cookbook" /> */}
+              <Menu.Menu position="right">
+                <Menu.Item
+                  name="search"
+                  position="right"
+                  onChange={this.handleOnChange}
+                >
+                  <BasicSearch
+                    history={this.props.history}
+                    onRecipeInput={this.props.onRecipeInput}
+                    redirect={true}
+                  />
+                </Menu.Item>
 
-  
-          <Menu.Item as={Link} to="/">
-              <Icon color='orange' name='home' size='big' />
-          </Menu.Item>
-          {/* <Menu.Item as={Link} to="/" name="Dashboard" /> */}
-          <Menu.Item as={Link} to="/recipes">
-              <Icon color='orange' name='book' size='big' />
-          </Menu.Item>
-          {/* <Menu.Item as={Link} to="/recipes" name="Cookbook" /> */}
-          <Menu.Menu position="right">
-            <Menu.Item
-              name="search"
-              position="right"
-              onChange={this.handleOnChange}
-            >
-              <BasicSearch
-                history={this.props.history}
-                onRecipeInput={this.props.onRecipeInput}
-                redirect={true}
-              />
-            </Menu.Item>
+                <Menu.Item name="navigate">{<NavDrop />}</Menu.Item>
 
-            <Menu.Item name="navigate">{<NavDrop />}</Menu.Item>
-
-            <Menu.Item name="logout" onClick={this.handleLogOut} />
-          </Menu.Menu>
-          
+                <Menu.Item name="logout" onClick={this.handleLogOut} />
+              </Menu.Menu>
+            </>
+          ) : null}
         </Menu>
       </div>
     );
